adaa8d05e36541402eddaa7f927d7cfe2fe8e951
John HP <john.k.gitonga@gmail.com>
2017-10-15 11:37:24 -0700

    Initial Commit

---
+++ src/main/java/jpUniversity/jpUniversity/JpUniversityApplication.java
@@ -0,0 +1,12 @@
+package jpUniversity.jpUniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpUniversityApplication.class, args);
+	}
+}
